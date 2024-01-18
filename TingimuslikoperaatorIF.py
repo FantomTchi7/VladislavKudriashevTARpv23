4fdd9a139eed628af2238e9bb0306db088f479b8
fantomtchi7 <fantomcorp@outlook.com>
2024-01-18 19:38:30 +0200

    Did some tasks, still tired as hell though.

--- TingimuslikoperaatorIF.py
+++ TingimuslikoperaatorIF.py
@@ -1,4 +1,6 @@
-print("Mis sinu nimi on?")
+# Ülesanne 1
+
+print("Mis on sinu nimi?")
 nimi=input()
 if nimi.capitalize()=="Juku":
     print("Kui vana Juku on?")
@@ -6,10 +8,36 @@ if nimi.capitalize()=="Juku":
     if vanus<0 or vanus>100:
         print("Yuku pole "+str(vanus)+"-aastane.")
     elif vanus<6:
-        print("tasuta pilet")
+        print("Te peate ostma tasuta pileti.")
     elif vanus<=14:
-        print("lastepilet")
+        print("Te peate ostma lastepileti.")
     elif vanus<=65:
-        print("täispilet")
+        print("Te peate ostma täispileti.")
     else:
-        print("sooduspilet")
+        print("Te peate ostma sooduspileti.")
+
+# Ülesanne 2
+
+print("Mis on esimese naabri nimi?")
+nimi1=str(input())
+print("Mis on teise naabri nimi?")
+nimi2=str(input())
+print("Täna "+nimi1+" ja "+nimi2+" on pinginaabrid.")
+
+# Ülesanne 3
+
+print("Kui pikk on esimene sein (meetrites)?")
+sein1=int(input())
+print("Kui pikk on teine sein (meetrites)?")
+sein2=int(input())
+põrand=sein1*sein2
+print("Teie põranda suurus on "+str(põrand)+"m²")
+print("Kas soovite renoveerimist?")
+nõus=str(input())
+if nõus.capitalize()=="Jah":
+    print("Kui palju maksab ruutmeeter?")
+    maksruut=int(input())
+    hind=maksruut*põrand
+    print("Põranda vahetamise hind on "+str(hind)+"€")
+
+# Ülesanne 4
