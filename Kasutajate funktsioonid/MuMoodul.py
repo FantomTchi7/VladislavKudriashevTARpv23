ce9c947cba6a2ffdbc85d816d68fb6cc21d2dbef
FantomTchi7 <fantomcorp@outlook.com>
2024-04-12 16:04:40 +0300

    Reorganized stuff

--- Kasutajate funktsioonid/MuMoodul.py
+++ Kasutajate funktsioonid/MuMoodul.py
@@ -1,6 +1,8 @@
 import string,random,smtplib,ssl
 from email.message import EmailMessage
 
+# Практическая работа "Создание пользовательских функций"
+
 def summa(arv1:int,arv2:int,arv3:int=0)->int:
     """ Funktsioon tagastab kolme arvu summa
         summa(arv1,arv2,arv3)
@@ -197,12 +199,32 @@ def palk_nimi_jargi(inimesed:list,palgad:list)->list:
 
 # Iseseisevtöö "Registreerimine ja autoriseerimine"
 
-def konto_loomine(nimed:list,paroolid:list)->any:
-    """Selleks kulub kaks loendit, inimeste paroolid, väljundis redigeeritakse neid ja luuakse kasutaja, kus kasutajal ja paroolil on sama indeks.
-    
-    :param list nimed: Nimete järjend
+def parooli_keerukuse_kontroll(password):
+    """Kontrollib, kas antud parool vastab keerukuse kriteeriumidele.
+
+    :param str parool: Kontrollitav parool.
+    :return: Tuple, kus esimene element on True või False, olenevalt sellest, kas parool vastab kriteeriumidele või mitte. Teine element on vastav sõnum vastavalt kontrolli tulemusele.
+    :rtype: tuple
+    """
+    if len(password)<8:
+        return False,"Parool peab olema vähemalt 8 tähemärki pikk."
+    if not any(char.isupper() for char in password):
+        return False,"Parool peab sisaldama vähemalt ühte suurtähte."
+    if not any(char.islower() for char in password):
+        return False,"Parool peab sisaldama vähemalt ühte väiketähte."
+    if not any(char.isdigit() for char in password):
+        return False,"Parool peab sisaldama vähemalt ühte numbrit."
+    if not any(char in string.punctuation for char in password):
+        return False,"Parool peab sisaldama vähemalt ühte erimärki."
+    return True,"Parool vastab keerukuse nõuetele."
+
+def konto_loomine(nimed:list,paroolid:list,emails:list)->any:
+    """Funktsioon loob kasutaja konto, kus kasutajal ja paroolil on sama indeks.
+
+    :param list nimed: Nimede järjend
     :param list paroolid: Paroolide järjend
-    :rtype: list,list
+    :param list emails: Emailide järjend
+    :rtype: tuple
     """
     nimi=input("Sisesta nimi: ")
     if nimi in nimed:
@@ -210,51 +232,53 @@ def konto_loomine(nimed:list,paroolid:list)->any:
     else:
         valik=int(input("1) Sisestage parool ise. 2) Looge juhuslikult parool.\n"))
         if valik==1:
-            parool=input("Sisesta parool: ")
+            while True:
+                parool=input("Sisesta parool: ")
+                keeruline,sonum=parooli_keerukuse_kontroll(parool)
+                if keeruline:
+                    break
+                else:
+                    print(sonum)
         elif valik==2:
-            randomparool=list(string.printable)
-            random.shuffle(randomparool)
-            parool = ''.join([random.choice(randomparool) for x in range(12)])
-            print(parool)
-            randomparool=list(string.printable)
-            random.shuffle(randomparool)
-            parool = ''.join([random.choice(randomparool) for x in range(12)])
+            parool=''.join(random.choices(string.ascii_letters+string.digits,k=12))
             print(parool)
         nimed.append(nimi)
         paroolid.append(parool)
+        email=input("Sisesta email: ")
+        emails.append(email)
         print("Konto loomine õnnestus.")
-        print([nimi,parool])
-    return nimed,paroolid
+        print(nimi,parool,email)
+    return nimed,paroolid,emails
 
 def kontole_sisse_logida(nimed:list,paroolid:list,on_autoriseeritud:str)->str:
-    """Konto sisselogimise funktsioon kontrollib loendis olevaid kasutajaid, tagastab sisestatud kasutaja nime "on_autoriseeritud".
-    
-    :param list nimed: Nimete järjend
+    """Funktsioon kontrollib loendis olevaid kasutajaid ja tagastab sisselogitud kasutaja nime.
+
+    :param list nimed: Nimede järjend
     :param list paroolid: Paroolide järjend
-    :param str on_autoriseeritud: Sisselogitud kasutaja nimi; kui kontole sisselogimist pole, tagastab see "False"
+    :param str on_autoriseeritud: Sisselogitud kasutaja nimi; kui kontole sisselogimist pole, tagastab "False"
     :rtype: str
     """
-    if on_autoriseeritud=="False":
-        nimi=input("Sisesta nimi: ")
-        if not(nimi in nimed):
-            print("Selle nimega kontot pole.")
-        for i in range(len(nimed)):
-            if nimed[i]==nimi:
-                parool=input("Sisesta parool: ")
-                if paroolid[i]==parool:
-                    print("Login successful")
-                    on_autoriseeritud=nimi
-                else:
-                    print("Vale parool.")
+    nimi=input("Sisesta nimi: ")
+    if not(nimi in nimed):
+        print("Selle nimega kontot pole.")
+    for i in range(len(nimed)):
+        if nimed[i]==nimi:
+            parool=input("Sisesta parool: ")
+            if paroolid[i]==parool:
+                print("Login successful")
+                on_autoriseeritud=nimi
+            else:
+                print("Vale parool.")
     return on_autoriseeritud
 
-def muuda_salasona(nimed:list,paroolid:list,on_autoriseeritud:str)->any:
-    """Sisselogitud kasutaja parooli muutmine.
-    
-    :param list nimed: Nimete järjend
+def muuda_salasona(nimed:list,paroolid:list,emails:list,on_autoriseeritud:str)->any:
+    """Funktsioon muudab sisselogitud kasutaja parooli.
+
+    :param list nimed: Nimede järjend
     :param list paroolid: Paroolide järjend
+    :param list emails: Emailide järjend
     :param str on_autoriseeritud: Kontrollib, kas kasutaja on sisse logitud
-    :rtype: list,list
+    :rtype: tuple
     """
     if not(on_autoriseeritud=="False"):
         indeks=nimed.index(on_autoriseeritud)
@@ -262,28 +286,36 @@ def muuda_salasona(nimed:list,paroolid:list,on_autoriseeritud:str)->any:
         if parool==paroolid[indeks]:
             valik=int(input("1) Sisestage parool ise. 2) Looge juhuslikult parool.\n"))
             if valik==1:
-                parool=input("Sisesta uus parool: ")
+                while True:
+                    parool=input("Sisesta parool: ")
+                    keeruline,sonum=parooli_keerukuse_kontroll(parool)
+                    if keeruline:
+                        break
+                    else:
+                        print(sonum)
             elif valik==2:
-                randomparool=list(string.printable)
-                random.shuffle(randomparool)
-                parool = ''.join([random.choice(randomparool) for x in range(12)])
+                parool=''.join(random.choices(string.ascii_letters+string.digits,k=12))
                 print(parool)
             nimi=nimed[indeks]
+            email=emails[indeks]
             nimed.pop(indeks)
             paroolid.pop(indeks)
+            emails.pop(indeks)
             nimed.append(nimi)
             paroolid.append(parool)
+            emails.append(email)
         else:
             print("Vale parool.")
     else:
         print("Pole autoriseeritud")
-    return nimed,paroolid
+    return nimed,paroolid,emails
 
-def muuda_nimi(nimed:list,paroolid:list,on_autoriseeritud:str)->str:
-    """Muudab kasutajanime ja lisab selle uusimasse indeksisse. Tagastab uue kasutajanime "on_autoriseritud".
-    
-    :param list nimed: Nimete järjend
+def muuda_nimi(nimed:list,paroolid:list,emails:list,on_autoriseeritud:str)->str:
+    """Funktsioon muudab kasutajanime ja tagastab uue kasutajanime.
+
+    :param list nimed: Nimede järjend
     :param list paroolid: Paroolide järjend
+    :param list emails: Emailide järjend
     :param str on_autoriseeritud: Kontrollib, kas kasutaja on sisse logitud
     :rtype: str
     """
@@ -293,10 +325,13 @@ def muuda_nimi(nimed:list,paroolid:list,on_autoriseeritud:str)->str:
         if parool==paroolid[indeks]:
             nimi=input("Sisesta uus nimi: ")
             parool=paroolid[indeks]
+            email=emails[indeks]
             paroolid.pop(indeks)
             nimed.pop(indeks)
+            emails.pop(indeks)
             nimed.append(nimi)
             paroolid.append(parool)
+            emails.append(email)
             on_autoriseeritud=nimi
         else:
             print("Vale parool.")
@@ -304,49 +339,100 @@ def muuda_nimi(nimed:list,paroolid:list,on_autoriseeritud:str)->str:
         print("Pole autoriseeritud")
     return on_autoriseeritud
 
-def parooli_taastamine(nimed:list,paroolid:list,on_autoriseeritud:str)->any:
-    """
-    
-    :param list nimed: Nimete järjend
+def parooli_taastamine(nimed:list,paroolid:list,emails:list,on_autoriseeritud:str)->any:
+    """Funktsioon taastab parooli ja saadab selle e-kirjana.
+
+    :param list nimed: Nimede järjend
     :param list paroolid: Paroolide järjend
+    :param list emails: Emailide järjend
     :param str on_autoriseeritud: Kontrollib, kas kasutaja on sisse logitud
-    :rtype: list,list
     """
     if not(on_autoriseeritud=="False"):
         indeks=nimed.index(on_autoriseeritud)
-        parool=input("Sisesta oma parool: ")
-        if parool==paroolid[indeks]:
-            valik=int(input("1) Sisestage parool ise. 2) Looge juhuslikult parool.\n"))
-            if valik==1:
-                parool=input("Sisesta uus parool: ")
-            elif valik==2:
-                randomparool=list(string.printable)
-                random.shuffle(randomparool)
-                parool = ''.join([random.choice(randomparool) for x in range(12)])
-                print(parool)
-            to_email=input("Sisesta oma email: ")
+        valik=int(input("1) Sisestage parool ise. 2) Looge juhuslikult parool.\n"))
+        if valik==1:
+            while True:
+                parool=input("Sisesta parool: ")
+                keeruline,sonum=parooli_keerukuse_kontroll(parool)
+                if keeruline:
+                    break
+                else:
+                    print(sonum)
+        elif valik==2:
+            parool=''.join(random.choices(string.ascii_letters+string.digits,k=12))
+        else:
+            print("Vigane valik.")
+        paroolid[indeks]=parool
+        try:
             smtp_server="smtp.gmail.com"
             port=587
-            sender_email="othermodstactics@gmail.com"
-            password="rdle rkdb xmbu fygl"
+            sender_email=emails[indeks]
+            password="alar oalu xkvu wycl"
+            to_email="othermodstactics@gmail.com"
             context=ssl.create_default_context()
-            msg=EmailMessage()
-            msg.set_content(f"Sinu uus parool on {parool}")
-            msg['Subject']="Sinu uus parool!"
-            msg['From']=sender_email
-            msg['To']=to_email
-            try:
-                server = smtplib.SMTP(smtp_server,port)
+            with smtplib.SMTP(smtp_server, port) as server:
                 server.ehlo()
                 server.starttls(context=context)
-                server.ehlo()
-                server.login(sender_email, password)
-                server.sendmail(msg)
-            except Exception as e:
-                print(e)
-            finally:
-                server.quit()
-        else:
-            print("Vale parool.")
+                server.login(sender_email,password)
+                msg=EmailMessage()
+                msg.set_content(f"Sinu uus parool on {parool}")
+                msg['Subject']="Sinu uus parool!"
+                msg['From']=sender_email
+                msg['To']=to_email
+                server.send_message(msg)
+            print("E-kiri saadetud edukalt.")
+        except Exception as e:
+            print(f"E-kirja saatmisel tekkis viga: {e}")
     else:
         print("Pole autoriseeritud")
+
+def loe_failist(fail:str)->list:
+    """Funktsioon loeb failist read ja salvestab need järjendisse. Tagastab järjendi.
+
+    :param str fail: Faili nimi
+    :rtype: list
+    """
+    try:
+        f=open(fail,'r',encoding="utf-8")
+        järjend=[]
+        for rida in f:
+            järjend.append(rida.strip())
+        f.close()
+    except Exception as e:
+        print(e)
+    return järjend
+
+def kirjuta_failisse(fail: str, nimed: list, paroolid: list, emails: list):
+    """Funktsioon kirjutab andmed faili.
+
+    :param str fail: Faili nimi
+    :param list nimed: Kasutajanimede järjend
+    :param list paroolid: Paroolide järjend
+    :param list emails: Emailide järjend
+    """
+    try:
+        f=open(fail,'w',encoding="utf-8")
+        for nimi,parool,email in zip(nimed,paroolid,emails):
+            f.write(f"{nimi}:{parool}|{email}\n")
+        f.close()
+    except Exception as e:
+        print(e)
+
+def loe_pas_ja_log(fail: str) -> tuple:
+    """Funktsioon loeb failist andmed, mis on formaadis "login:password:email" igas reas eraldi.
+
+    :param str fail: Faili nimi
+    :rtype: tuple
+    """
+    fail=open(fail,"r",encoding="utf-8")
+    log=[]
+    pas=[]
+    mail=[]
+    for line in fail:
+        n=line.find(":")
+        p=line.find("|")
+        log.append(line[0:n].strip())
+        pas.append(line[n+1:p].strip())
+        mail.append(line[p+1:].strip())
+    fail.close()
+    return log,pas,mail
