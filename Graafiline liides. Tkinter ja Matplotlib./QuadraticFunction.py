428c5ad5329a8be7da375f83890d1d5e085a33c8
FantomTchi7 <fantomcorp@outlook.com>
2024-04-12 12:41:49 +0300

    I'm in pain

--- Graafiline liides. Tkinter ja Matplotlib./QuadraticFunction.py
+++ Graafiline liides. Tkinter ja Matplotlib./QuadraticFunction.py
@@ -20,12 +20,12 @@ def solve():
     if not (aa=="" or bb=="" or cc==""):
         D=round((float(bb)**2)-(4*float(aa)*float(cc)),roundTo)
         if D>0:
-            x1=round((abs(float(bb))+math.sqrt(D))/(2*float(aa)),roundTo)
-            x2=round((abs(float(bb))-math.sqrt(D))/(2*float(aa)),roundTo)
+            x1=round((-(float(bb))+math.sqrt(D))/(2*float(aa)),roundTo)
+            x2=round((-(float(bb))-math.sqrt(D))/(2*float(aa)),roundTo)
             solution.configure(text=f"D={D}\nX₁={x1}\nX₂={x2}")
             graph()
         elif D==0:
-            x=round(abs(float(bb))/(2*float(aa)),roundTo)
+            x=round(-(float(bb))/(2*float(aa)),roundTo)
             solution.configure(text=f"D={D}\nX={x}")
             graph()
         else:
@@ -39,7 +39,7 @@ def graph():
     aa=float(a.get())
     bb=float(b.get())
     cc=float(c.get())
-    x=round(abs(bb)/(2*aa),roundTo)
+    x=round(-(bb)/(2*aa),roundTo)
     x1=numpy.arange(x-step,x+step,float(f"0.{step}"))
     y=aa*x**2+bb*x1+cc
     y1=aa*x1**2+bb*x1+cc
