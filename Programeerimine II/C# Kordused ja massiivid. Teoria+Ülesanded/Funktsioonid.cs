cc00c78a7e8a710b41486319cfe7b8cf778b9252
FantomTchi7 <opilane@tthk.ee>
2024-08-28 14:18:36 +0300

    :chosoNotUnderstand:

---
+++ Programeerimine II/C# Kordused ja massiivid. Teoria+Ülesanded/Funktsioonid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KudriashevTARpv23
+{
+    internal class Funktsioonid
+    {
+        Random random = new Random();
+        public static int[] Arvude_massiiv(int n, int m)
+        {
+            int arv;
+            int[] arvud = new int[m-n];
+            for (int i = 0; i < arvud.Length; i++)
+            {
+                arv = n;
+                arvud[i] = arv;
+                n++;
+            }
+            return arvud;
+        }
+        public static void viis_Arvud_Massiiv()
+        {
+            int[] arvud = new int[];
+            for (int i = 0; i < 5; i++)
+            {
+                int arv = int.Parse(Console.ReadLine());
+                int[i]=arv
+            }
+        }
+    }
+}
---
+++ Programeerimine II/C# Kordused ja massiivid. Teoria+Ülesanded/MainClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KudriashevTARpv23
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int n = random.Next(-100, 101);
+            int m = random.Next(-100, 101);
+            int[] arvud;
+            if (n < m)
+            {
+                arvud = Funktsioonid.Arvude_massiiv(n, m);
+            }
+            else
+            {
+                arvud = Funktsioonid.Arvude_massiiv(m, n);
+            }
+            foreach (int arv in arvud)
+            {
+                Console.WriteLine(arv*arv);
+            }
+        }
+    }
+}
