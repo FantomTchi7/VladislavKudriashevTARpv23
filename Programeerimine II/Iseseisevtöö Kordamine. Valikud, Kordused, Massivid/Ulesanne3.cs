1c956ee6694f0c0b21220367a81dec8eeb56495e
FantomTchi7 <opilane@HP-CZC2349HV0>
2024-09-06 14:16:25 +0300

    :chosoInVain:

---
+++ Programeerimine II/Iseseisevtöö Kordamine. Valikud, Kordused, Massivid/Ulesanne3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KudriashevTARpv23
+{
+    internal class Ulesanne3
+    {
+        public static void Ulesanne()
+        {
+            Random random = new Random();
+
+            int rows = random.Next(1, 10);
+            int columns = random.Next(1, 20);
+
+            string[,] seatings = new string[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    seatings[i, j] = random.Next(2).ToString();
+                }
+            }
+
+        }
+    }
+}
