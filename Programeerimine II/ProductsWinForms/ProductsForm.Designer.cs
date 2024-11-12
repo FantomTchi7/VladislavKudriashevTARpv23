899a19ecd72aea35de6790361af59756d0adf63e
Opilane TTHK <opilane@tthk.ee>
2024-11-12 14:49:20 +0200

    DataBase stuff

---
+++ Programeerimine II/ProductsWinForms/ProductsForm.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ProductsWinForms
+{
+    partial class ProductsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridViewProducts = new System.Windows.Forms.DataGridView();
+            this.productIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productAmountDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productPriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productsDataSet = new ProductsWinForms.ProductsDataSet();
+            this.productsTableAdapter = new ProductsWinForms.ProductsDataSetTableAdapters.ProductsTableAdapter();
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelAmount = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.textBoxAmount = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.labelProductID = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewProducts
+            // 
+            this.dataGridViewProducts.AutoGenerateColumns = false;
+            this.dataGridViewProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProducts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productIDDataGridViewTextBoxColumn,
+            this.productNameDataGridViewTextBoxColumn,
+            this.productAmountDataGridViewTextBoxColumn,
+            this.productPriceDataGridViewTextBoxColumn});
+            this.dataGridViewProducts.DataSource = this.productsBindingSource;
+            this.dataGridViewProducts.Location = new System.Drawing.Point(28, 129);
+            this.dataGridViewProducts.Name = "dataGridViewProducts";
+            this.dataGridViewProducts.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewProducts.TabIndex = 0;
+            // 
+            // productIDDataGridViewTextBoxColumn
+            // 
+            this.productIDDataGridViewTextBoxColumn.DataPropertyName = "ProductID";
+            this.productIDDataGridViewTextBoxColumn.HeaderText = "ProductID";
+            this.productIDDataGridViewTextBoxColumn.Name = "productIDDataGridViewTextBoxColumn";
+            this.productIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // productNameDataGridViewTextBoxColumn
+            // 
+            this.productNameDataGridViewTextBoxColumn.DataPropertyName = "ProductName";
+            this.productNameDataGridViewTextBoxColumn.HeaderText = "ProductName";
+            this.productNameDataGridViewTextBoxColumn.Name = "productNameDataGridViewTextBoxColumn";
+            // 
+            // productAmountDataGridViewTextBoxColumn
+            // 
+            this.productAmountDataGridViewTextBoxColumn.DataPropertyName = "ProductAmount";
+            this.productAmountDataGridViewTextBoxColumn.HeaderText = "ProductAmount";
+            this.productAmountDataGridViewTextBoxColumn.Name = "productAmountDataGridViewTextBoxColumn";
+            // 
+            // productPriceDataGridViewTextBoxColumn
+            // 
+            this.productPriceDataGridViewTextBoxColumn.DataPropertyName = "ProductPrice";
+            this.productPriceDataGridViewTextBoxColumn.HeaderText = "ProductPrice";
+            this.productPriceDataGridViewTextBoxColumn.Name = "productPriceDataGridViewTextBoxColumn";
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.productsDataSet;
+            // 
+            // productsDataSet
+            // 
+            this.productsDataSet.DataSetName = "ProductsDataSet";
+            this.productsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.AutoSize = true;
+            this.labelProductName.Location = new System.Drawing.Point(25, 26);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(45, 13);
+            this.labelProductName.TabIndex = 1;
+            this.labelProductName.Text = "Nimetus";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(76, 22);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // labelAmount
+            // 
+            this.labelAmount.AutoSize = true;
+            this.labelAmount.Location = new System.Drawing.Point(25, 51);
+            this.labelAmount.Name = "labelAmount";
+            this.labelAmount.Size = new System.Drawing.Size(37, 13);
+            this.labelAmount.TabIndex = 3;
+            this.labelAmount.Text = "Kogus";
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(25, 77);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(29, 13);
+            this.labelPrice.TabIndex = 4;
+            this.labelPrice.Text = "Hind";
+            // 
+            // textBoxAmount
+            // 
+            this.textBoxAmount.Location = new System.Drawing.Point(76, 48);
+            this.textBoxAmount.Name = "textBoxAmount";
+            this.textBoxAmount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAmount.TabIndex = 5;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(76, 74);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 6;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.AutoSize = true;
+            this.buttonInsert.Location = new System.Drawing.Point(28, 100);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(77, 23);
+            this.buttonInsert.TabIndex = 7;
+            this.buttonInsert.Text = "Lisa andmed";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.AutoSize = true;
+            this.buttonDelete.Location = new System.Drawing.Point(111, 100);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(94, 23);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Kustuta andmed";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // labelProductID
+            // 
+            this.labelProductID.AutoSize = true;
+            this.labelProductID.Location = new System.Drawing.Point(182, 25);
+            this.labelProductID.Name = "labelProductID";
+            this.labelProductID.Size = new System.Drawing.Size(18, 13);
+            this.labelProductID.TabIndex = 9;
+            this.labelProductID.Text = "ID";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(206, 22);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(100, 20);
+            this.textBoxID.TabIndex = 10;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.AutoSize = true;
+            this.buttonUpdate.Location = new System.Drawing.Point(206, 48);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(96, 23);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Uuenda andmed";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // ProductsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.labelProductID);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxAmount);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.labelAmount);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelProductName);
+            this.Controls.Add(this.dataGridViewProducts);
+            this.Name = "ProductsForm";
+            this.Text = "Products Form";
+            this.Load += new System.EventHandler(this.ProductsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewProducts;
+        private ProductsDataSet productsDataSet;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private ProductsDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productAmountDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productPriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label labelProductName;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelAmount;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.TextBox textBoxAmount;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label labelProductID;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Button buttonUpdate;
+    }
+}
+
---
+++ Programeerimine II/ProductsWinForms/ProductsForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ProductsWinForms
+{
+    public partial class ProductsForm : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Products;Integrated Security=True");
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        public ProductsForm()
+        {
+            InitializeComponent();
+        }
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            command = new SqlCommand("INSERT INTO Products(ProductName, ProductAmount, ProductPrice) VALUES (@Name, @Amount, @Price)", connection);
+            command.Parameters.AddWithValue("@Name", textBoxName.Text);
+            command.Parameters.AddWithValue("@Amount", textBoxAmount.Text);
+            command.Parameters.AddWithValue("@Price", textBoxPrice.Text);
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            productsTableAdapter.Fill(productsDataSet.Products);
+        }
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            command = new SqlCommand("DELETE FROM Products WHERE ProductName = @Name AND ProductAmount = @Amount AND ProductPrice = @Price", connection);
+            command.Parameters.AddWithValue("@Name", textBoxName.Text);
+            command.Parameters.AddWithValue("@Amount", textBoxAmount.Text);
+            command.Parameters.AddWithValue("@Price", textBoxPrice.Text);
+            command.ExecuteNonQuery();
+
+            command = new SqlCommand("DBCC CHECKIDENT ('Products', RESEED, 0)", connection);
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            productsTableAdapter.Fill(productsDataSet.Products);
+        }
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            command = new SqlCommand("UPDATE Products SET ProductName = @Name, ProductAmount = @Amount, ProductPrice = @Price WHERE ProductID = @ID", connection);
+            command.Parameters.AddWithValue("@ID", textBoxID.Text);
+            command.Parameters.AddWithValue("@Name", textBoxName.Text);
+            command.Parameters.AddWithValue("@Amount", textBoxAmount.Text);
+            command.Parameters.AddWithValue("@Price", textBoxPrice.Text);
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            productsTableAdapter.Fill(productsDataSet.Products);
+        }
+        private void ProductsForm_Load(object sender, EventArgs e)
+        {
+            dataGridViewProducts.AutoSize = true;
+            Width = dataGridViewProducts.Width + 25;
+            // TODO: This line of code loads data into the 'productsDataSet.Products' table. You can move, or remove it, as needed.
+            productsTableAdapter.Fill(productsDataSet.Products);
+        }
+    }
+}
