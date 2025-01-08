74a3d8265f57c7dc06b8f5975e90128fde325f6c
Opilane TTHK <opilane@tthk.ee>
2025-01-08 11:33:35 +0200

    Kutse App 1

---
+++ Programeerimine II/KutseApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using KutseApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KutseApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Ootan sind minu peole! Palun tule!!!";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public ViewResult Ankeet()
+        {
+            return View();
+        }
+    }
+}
